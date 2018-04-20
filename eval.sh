9dc03e9563db67f20d1cd9d02cf83db28560a975
mhassan8119 <hassanmailbox4@gmail.com>
2018-04-20 10:31:13 +0200

    :q

--- eval.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PIN=/home/hassan/Downloads/pin-3.6-97554-g31f0a167d-gcc-linux/pin
-#zlib_flux= /home/gvaumour/Dev/apps/traces-pintools/zlib_flux/convert_flux
-
-#$PIN -ifeellucky -injection child -t ./obj-intel64/roeval.so -- ./test/helloWorld/test
-
-$PIN -t ./obj-intel64/roeval.so -- ./a
-
--- src/roeval.hh
+++
@@ -1,4 +0,0 @@
-#ifndef ROEVAL_HH_
-#define ROEVAL_HH_
-
-#endif
